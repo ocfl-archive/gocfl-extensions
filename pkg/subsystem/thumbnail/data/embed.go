68f07c41038310a0c2aae5f24686aec7c15c71bc
Juergen Enge <you@example.com>
2026-04-22 18:05:03 +0200

    Refine embedded scripts for thumbnail generation

--- pkg/subsystem/thumbnail/data/embed.go
+++ pkg/subsystem/thumbnail/data/embed.go
@@ -8,5 +8,5 @@ import (
 //go:embed thumbnail.toml
 var ThumbnailConfigString string
 
-//go:embed scripts/*
+//go:embed scripts/pdf2thumb.ps1 scripts/pdf2thumb.sh scripts/video2thumb.ps1 scripts/video2thumb.sh
 var ThumbnailFS embed.FS
