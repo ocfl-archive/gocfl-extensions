fef08faac558a3197b5eff83fe723704d437c187
Juergen Enge <you@example.com>
2026-05-04 18:48:00 +0200

    Add `ext_0012_hash_and_no_prefix_id_n_tuple_storage_layout` extension with implementation, tests, and documentation.

---
+++ pkg/extension/ext_0012_hash_and_no_prefix_id_n_tuple_storage_layout/0012-hash-and-no-prefix-id-n-tuple-storage-layout.go
@@ -0,0 +1,205 @@
+package ext_0012_hash_and_no_prefix_id_n_tuple_storage_layout
+
+import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"emperror.dev/errors"
+	"github.com/je4/filesystem/v3/pkg/writefs"
+	"github.com/je4/utils/v2/pkg/checksum"
+	"github.com/ocfl-archive/gocfl/v3/pkg/appendfs"
+	"github.com/ocfl-archive/gocfl/v3/pkg/ocfl/extension"
+	"github.com/ocfl-archive/gocfl/v3/pkg/ocfl/storageroot"
+	"github.com/ocfl-archive/gocfl/v3/pkg/ocfllogger"
+)
+
+const StorageLayoutHashAndNoPrefixIdNTupleName = "0012-hash-and-no-prefix-id-n-tuple-storage-layout"
+const StorageLayoutHashAndNoPrefixIdNTupleDescription = "Hashed Truncated N-tuple Trees with Non-prefixed Object ID Encapsulating Directory for OCFL Storage Hierarchies"
+
+//go:embed 0012-hash-and-no-prefix-id-n-tuple-storage-layout.md
+var StorageLayoutHashAndNoPrefixIdNTupleDoc string
+
+func init() {
+	extension.RegisterExtension(StorageLayoutHashAndNoPrefixIdNTupleName, NewStorageLayoutHashAndNoPrefixIdNTuple, nil, &StorageLayoutHashAndNoPrefixIdNTupleDoc)
+}
+
+func NewStorageLayoutHashAndNoPrefixIdNTuple() (extension.Extension, error) {
+	config := &StorageLayoutHashAndNoPrefixIdNTupleConfig{
+		ExtensionConfig: &extension.ExtensionConfig{ExtensionName: StorageLayoutHashAndNoPrefixIdNTupleName},
+		DigestAlgorithm: string(checksum.DigestSHA256),
+		TupleSize:       3,
+		NumberOfTuples:  3,
+		Delimiters:      []string{},
+	}
+	sl := &StorageLayoutHashAndNoPrefixIdNTuple{StorageLayoutHashAndNoPrefixIdNTupleConfig: config}
+	return sl, nil
+}
+
+func (sl *StorageLayoutHashAndNoPrefixIdNTuple) WithLogger(logger ocfllogger.OCFLLogger) extension.Extension {
+	sl.logger = logger.With("extension", StorageLayoutHashAndNoPrefixIdNTupleName)
+	return sl
+}
+
+func (sl *StorageLayoutHashAndNoPrefixIdNTuple) Load(data json.RawMessage) error {
+	if err := json.Unmarshal(data, sl.StorageLayoutHashAndNoPrefixIdNTupleConfig); err != nil {
+		return errors.Wrapf(err, "cannot unmarshal StorageLayoutHashAndNoPrefixIdNTupleConfig '%s'", string(data))
+	}
+	if sl.NumberOfTuples > 32 {
+		sl.NumberOfTuples = 32
+	}
+	if sl.TupleSize > 32 {
+		sl.TupleSize = 32
+	}
+	if (sl.TupleSize == 0 && sl.NumberOfTuples != 0) || (sl.TupleSize != 0 && sl.NumberOfTuples == 0) {
+		sl.NumberOfTuples = 0
+		sl.TupleSize = 0
+	}
+	return nil
+}
+
+type StorageLayoutHashAndNoPrefixIdNTupleConfig struct {
+	*extension.ExtensionConfig
+	DigestAlgorithm string   `json:"digestAlgorithm"`
+	TupleSize       int      `json:"tupleSize"`
+	NumberOfTuples  int      `json:"numberOfTuples"`
+	Delimiters      []string `json:"delimiters"`
+}
+
+type StorageLayoutHashAndNoPrefixIdNTuple struct {
+	*StorageLayoutHashAndNoPrefixIdNTupleConfig
+	logger ocfllogger.OCFLLogger
+}
+
+func (sl *StorageLayoutHashAndNoPrefixIdNTuple) Terminate() error {
+	return nil
+}
+
+func (sl *StorageLayoutHashAndNoPrefixIdNTuple) GetConfig() any {
+	return sl.StorageLayoutHashAndNoPrefixIdNTupleConfig
+}
+
+func (sl *StorageLayoutHashAndNoPrefixIdNTuple) IsRegistered() bool {
+	return true
+}
+
+func (sl *StorageLayoutHashAndNoPrefixIdNTuple) GetName() string {
+	return StorageLayoutHashAndNoPrefixIdNTupleName
+}
+
+func (sl *StorageLayoutHashAndNoPrefixIdNTuple) SetParams(map[string]string) error {
+	return nil
+}
+
+func (sl *StorageLayoutHashAndNoPrefixIdNTuple) WriteConfig(fsys appendfs.FS) error {
+	configWriter, err := writefs.Create(fsys, "config.json")
+	if err != nil {
+		return errors.Wrap(err, "cannot open config.json")
+	}
+	defer configWriter.Close()
+	jenc := json.NewEncoder(configWriter)
+	jenc.SetIndent("", "   ")
+	if err := jenc.Encode(sl.StorageLayoutHashAndNoPrefixIdNTupleConfig); err != nil {
+		return errors.Wrapf(err, "cannot encode config to file")
+	}
+	return nil
+}
+
+func removePrefixes(objectID string, delimiters []string) string {
+	rightmostIdx := -1
+	for _, delimiter := range delimiters {
+		if len(delimiter) > 0 {
+			// Find delimiter in objectID, ignoring one at the very end
+			idx := strings.LastIndex(objectID[0:len(objectID)-1], delimiter)
+			if idx >= 0 {
+				if idx+len(delimiter) > rightmostIdx {
+					rightmostIdx = idx + len(delimiter)
+				}
+			}
+		}
+	}
+	if rightmostIdx > 0 {
+		return objectID[rightmostIdx:]
+	}
+	return objectID
+}
+
+func isSafe(c byte) bool {
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || c == '-' || c == '_'
+}
+
+func percentEncode(str string) string {
+	var result strings.Builder
+	for i := 0; i < len(str); i++ {
+		c := str[i]
+		if isSafe(c) {
+			result.WriteByte(c)
+		} else {
+			result.WriteString(fmt.Sprintf("%%%02x", c))
+		}
+	}
+	return result.String()
+}
+
+func getEncapsulationDirectory(objectIDWithNoPrefix string, digest string) string {
+	encoded := percentEncode(objectIDWithNoPrefix)
+	if len(encoded) > 100 {
+		return encoded[:100] + "-" + digest
+	}
+	return encoded
+}
+
+func (sl *StorageLayoutHashAndNoPrefixIdNTuple) BuildStorageRootPath(storageRoot storageroot.StorageRoot, id string) (string, error) {
+	idNoPrefix := removePrefixes(id, sl.Delimiters)
+
+	h, err := checksum.GetHash(checksum.DigestAlgorithm(sl.DigestAlgorithm))
+	if err != nil {
+		return "", errors.Wrapf(err, "cannot get hash for %s", sl.DigestAlgorithm)
+	}
+	if _, err := h.Write([]byte(idNoPrefix)); err != nil {
+		return "", errors.Wrapf(err, "cannot hash %s", idNoPrefix)
+	}
+	digestBytes := h.Sum(nil)
+	digest := fmt.Sprintf("%x", digestBytes)
+
+	if len(digest) < sl.TupleSize*sl.NumberOfTuples {
+		return "", errors.Errorf("digest %s too short for %v tuples of %v chars", sl.DigestAlgorithm, sl.NumberOfTuples, sl.TupleSize)
+	}
+
+	var dirparts []string
+	for i := 0; i < sl.NumberOfTuples; i++ {
+		dirparts = append(dirparts, digest[i*sl.TupleSize:(i+1)*sl.TupleSize])
+	}
+
+	encapsulationDir := getEncapsulationDirectory(idNoPrefix, digest)
+	dirparts = append(dirparts, encapsulationDir)
+
+	return strings.Join(dirparts, "/"), nil
+}
+
+func (sl *StorageLayoutHashAndNoPrefixIdNTuple) WriteLayout(fsys appendfs.FS) error {
+	configWriter, err := writefs.Create(fsys, "ocfl_layout.json")
+	if err != nil {
+		return errors.Wrap(err, "cannot open ocfl_layout.json")
+	}
+	defer configWriter.Close()
+	jenc := json.NewEncoder(configWriter)
+	jenc.SetIndent("", "   ")
+	if err := jenc.Encode(struct {
+		Extension   string `json:"extension"`
+		Description string `json:"description"`
+	}{
+		Extension:   StorageLayoutHashAndNoPrefixIdNTupleName,
+		Description: StorageLayoutHashAndNoPrefixIdNTupleDescription,
+	}); err != nil {
+		return errors.Wrapf(err, "cannot encode config to file")
+	}
+	return nil
+}
+
+// check interface satisfaction
+var (
+	_ extension.Extension                  = &StorageLayoutHashAndNoPrefixIdNTuple{}
+	_ storageroot.ExtensionStorageRootPath = &StorageLayoutHashAndNoPrefixIdNTuple{}
+)
---
+++ pkg/extension/ext_0012_hash_and_no_prefix_id_n_tuple_storage_layout/0012-hash-and-no-prefix-id-n-tuple-storage-layout_test.go
@@ -0,0 +1,169 @@
+package ext_0012_hash_and_no_prefix_id_n_tuple_storage_layout_test
+
+import (
+	"encoding/json"
+	"path"
+	"testing"
+
+	"github.com/je4/filesystem/v3/pkg/writefs"
+	extensionbase "github.com/ocfl-archive/gocfl-extensions/pkg/extension"
+	"github.com/ocfl-archive/gocfl/v3/pkg/ocfl/extension/extensionimpl"
+	"github.com/ocfl-archive/gocfl/v3/pkg/ocfl/storageroot"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStorageLayoutHashAndNoPrefixIdNTuple(t *testing.T) {
+	env := extensionbase.SetupTestEnv(t)
+
+	type testCase struct {
+		name            string
+		digestAlgorithm string
+		tupleSize       int
+		numberOfTuples  int
+		delimiters      []string
+		objectID        string
+		expectedPath    string
+	}
+
+	testCases := []testCase{
+		// Example 1
+		{
+			name:            "Example 1: object-01",
+			digestAlgorithm: "sha256",
+			tupleSize:       3,
+			numberOfTuples:  3,
+			delimiters:      []string{},
+			objectID:        "object-01",
+			expectedPath:    "3c0/ff4/240/object-01",
+		},
+		{
+			name:            "Example 1: ..hor/rib:le-$id",
+			digestAlgorithm: "sha256",
+			tupleSize:       3,
+			numberOfTuples:  3,
+			delimiters:      []string{},
+			objectID:        "..hor/rib:le-$id",
+			expectedPath:    "487/326/d8c/%2e%2ehor%2frib%3ale-%24id",
+		},
+		// Example 2
+		{
+			name:            "Example 2: object-01",
+			digestAlgorithm: "md5",
+			tupleSize:       2,
+			numberOfTuples:  15,
+			delimiters:      []string{"/"},
+			objectID:        "object-01",
+			expectedPath:    "ff/75/53/44/92/48/5e/ab/b3/9f/86/35/67/28/88/object-01",
+		},
+		{
+			name:            "Example 2: ..hor/rib:le-$id",
+			digestAlgorithm: "md5",
+			tupleSize:       2,
+			numberOfTuples:  15,
+			delimiters:      []string{"/"},
+			objectID:        "..hor/rib:le-$id",
+			expectedPath:    "5d/6e/4e/8c/b5/cd/0c/7a/8f/bf/65/c1/29/51/27/rib%3ale-%24id",
+		},
+		// Example 3
+		{
+			name:            "Example 3: object-01",
+			digestAlgorithm: "sha256",
+			tupleSize:       0,
+			numberOfTuples:  0,
+			delimiters:      []string{"/"},
+			objectID:        "object-01",
+			expectedPath:    "object-01",
+		},
+		{
+			name:            "Example 3: ..hor/rib:le-$id",
+			digestAlgorithm: "sha256",
+			tupleSize:       0,
+			numberOfTuples:  0,
+			delimiters:      []string{"/"},
+			objectID:        "..hor/rib:le-$id",
+			expectedPath:    "rib%3ale-%24id",
+		},
+		// Additional Python code tests
+		{
+			name:            "Python test: object-01 with '-' delimiter",
+			digestAlgorithm: "sha256",
+			tupleSize:       3,
+			numberOfTuples:  3,
+			delimiters:      []string{"-"},
+			objectID:        "object-01",
+			expectedPath:    "938/db8/c9f/01",
+		},
+		{
+			name:            "Python test: md5 defaults",
+			digestAlgorithm: "md5",
+			tupleSize:       3,
+			numberOfTuples:  3,
+			delimiters:      []string{},
+			objectID:        "object-01",
+			expectedPath:    "ff7/553/449/object-01",
+		},
+		{
+			name:            "Python test: md5 5/2",
+			digestAlgorithm: "md5",
+			tupleSize:       5,
+			numberOfTuples:  2,
+			delimiters:      []string{},
+			objectID:        "object-01",
+			expectedPath:    "ff755/34492/object-01",
+		},
+		{
+			name:            "Python test: special characters",
+			digestAlgorithm: "sha256",
+			tupleSize:       3,
+			numberOfTuples:  3,
+			delimiters:      []string{},
+			objectID:        "..Hor/rib:lè-$id",
+			expectedPath:    "373/529/21a/%2e%2eHor%2frib%3al%c3%a8-%24id",
+		},
+		{
+			name:            "Truncation test: long ID",
+			digestAlgorithm: "sha256",
+			tupleSize:       3,
+			numberOfTuples:  3,
+			delimiters:      []string{},
+			objectID:        "abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghija",
+			expectedPath:    "5cc/73e/648/abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij-5cc73e648fbcff136510e330871180922ddacf193b68fdeff855683a01464220",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			conf := struct {
+				ExtensionName   string   `json:"extensionName"`
+				DigestAlgorithm string   `json:"digestAlgorithm"`
+				TupleSize       int      `json:"tupleSize"`
+				NumberOfTuples  int      `json:"numberOfTuples"`
+				Delimiters      []string `json:"delimiters"`
+			}{
+				ExtensionName:   "0012-hash-and-no-prefix-id-n-tuple-storage-layout",
+				DigestAlgorithm: tc.digestAlgorithm,
+				TupleSize:       tc.tupleSize,
+				NumberOfTuples:  tc.numberOfTuples,
+				Delimiters:      tc.delimiters,
+			}
+
+			data, _ := json.MarshalIndent(conf, "", "  ")
+			configPath := path.Join("0012-hash-and-no-prefix-id-n-tuple-storage-layout", "config.json")
+			_, err := writefs.WriteFile(env.ConfigFS, configPath, data)
+			assert.NoError(t, err)
+
+			extensionFactory, err := extensionimpl.NewFactory(nil, env.ConfigFS, env.Logger)
+			assert.NoError(t, err)
+
+			genericExtensionManager, err := extensionFactory.LoadExtensionManager(env.ConfigFS)
+			assert.NoError(t, err)
+
+			sl, ok := genericExtensionManager.(storageroot.ExtensionStorageRootPath)
+			assert.True(t, ok, "Extension manager should implement storageroot.ExtensionStorageRootPath interface")
+
+			path, err := sl.BuildStorageRootPath(nil, tc.objectID)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.expectedPath, path)
+		})
+	}
+}
