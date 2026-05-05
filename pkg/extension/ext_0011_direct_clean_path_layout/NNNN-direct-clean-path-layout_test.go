d8510e6e191e7c31913313d992fe133d685d5742
Juergen Enge <you@example.com>
2026-05-05 08:32:55 +0200

    Add `ext_0011_direct_clean_path_layout` extension tests.

---
+++ pkg/extension/ext_0011_direct_clean_path_layout/NNNN-direct-clean-path-layout_test.go
@@ -0,0 +1,101 @@
+package ext_0011_direct_clean_path_layout
+
+import (
+	"encoding/json"
+	"path"
+	"testing"
+
+	"github.com/je4/filesystem/v3/pkg/writefs"
+	extensionbase "github.com/ocfl-archive/gocfl-extensions/pkg/extension"
+	"github.com/ocfl-archive/gocfl/v3/pkg/ocfl/extension"
+	"github.com/ocfl-archive/gocfl/v3/pkg/ocfl/extension/extensionimpl"
+	"github.com/ocfl-archive/gocfl/v3/pkg/ocfl/object"
+	"github.com/ocfl-archive/gocfl/v3/pkg/ocfl/storageroot"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLegacyDirectCleanPathLayout(t *testing.T) {
+	env := extensionbase.SetupTestEnv(t)
+
+	type testCase struct {
+		id   string
+		path string
+	}
+
+	runTest := func(name string, config *DirectCleanConfig, cases []testCase) {
+		t.Run(name, func(t *testing.T) {
+			data, _ := json.MarshalIndent(config, "", "  ")
+			configPath := path.Join(LegacyDirectCleanName, "config.json")
+			_, err := writefs.WriteFile(env.ConfigFS, configPath, data)
+			assert.NoError(t, err)
+
+			extensionFactory, err := extensionimpl.NewFactory(nil, env.ConfigFS, env.Logger)
+			assert.NoError(t, err)
+
+			genericExtensionManager, err := extensionFactory.LoadExtensionManager(env.ConfigFS)
+			assert.NoError(t, err)
+
+			sl, ok := genericExtensionManager.(storageroot.ExtensionStorageRootPath)
+			assert.True(t, ok, "Extension manager should implement storageroot.ExtensionStorageRootPath interface")
+
+			cl, ok := genericExtensionManager.(object.ExtensionObjectContentPath)
+			assert.True(t, ok, "Extension manager should implement object.ExtensionObjectContentPath interface")
+
+			for _, tc := range cases {
+				t.Run(tc.id, func(t *testing.T) {
+					p, err := sl.BuildStorageRootPath(nil, tc.id)
+					assert.NoError(t, err)
+					assert.Equal(t, tc.path, p)
+
+					p2, err := cl.BuildObjectManifestPath(tc.id, "")
+					assert.NoError(t, err)
+					assert.Equal(t, tc.path, p2)
+				})
+			}
+		})
+	}
+
+	// Example #1: encodeUTF == false
+	runTest("Example1_False", &DirectCleanConfig{
+		ExtensionConfig:             &extension.ExtensionConfig{ExtensionName: LegacyDirectCleanName},
+		MaxPathnameLen:              32000,
+		MaxPathSegmentLen:           127,
+		UTFEncode:                   false,
+		ReplacementString:           "_",
+		WhitespaceReplacementString: " ",
+		FallbackDigestAlgorithm:     "md5",
+		FallbackFolder:              "fallback",
+		NumberOfFallbackTuples:      2,
+		FallbackTupleSize:           1,
+	}, []testCase{
+		{"..hor_rib:lé-$id", "..hor_rib_lé-$id"},
+		{"info:fedora/object-01", "info_fedora/object-01"},
+		{"~ info:fedora/-obj#ec@t-\"01 ", "info_fedora/obj_ec_t-_01"},
+		{"/test/ ~/.../blah", "test/_../blah"},
+		{"https://hdl.handle.net/XXXXX/test/bl ah", "https_/hdl.handle.net/XXXXX/test/bl ah"},
+		{"abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij", "fallback/0/e/0eafabb38fa7f1583d1461afe980ebdc"},
+	})
+
+	// Example #2: encodeUTF == true
+	runTest("Example2_True", &DirectCleanConfig{
+		ExtensionConfig:             &extension.ExtensionConfig{ExtensionName: LegacyDirectCleanName},
+		MaxPathnameLen:              32000,
+		MaxPathSegmentLen:           127,
+		UTFEncode:                   true,
+		ReplacementString:           "_",
+		WhitespaceReplacementString: " ",
+		FallbackDigestAlgorithm:     "sha512",
+		FallbackFolder:              "fallback",
+		NumberOfFallbackTuples:      2,
+		FallbackTupleSize:           1,
+	}, []testCase{
+		{"..hor_rib:lé-$id", "..hor_rib=u003Alé-$id"},
+		{"object=u123a-01", "object=u003Du123a-01"},
+		{"object=u13a-01", "object=u13a-01"},
+		{"info:fedora/object-01", "info=u003Afedora/object-01"},
+		{"~ info:fedora/-obj#ec@t-\"01 ", "=u007E=u0020info=u003Afedora/-obj=u0023ec=u0040t-=u002201=u0020"},
+		{"/test/ ~/.../blah", "test/=u0020~/=u002E../blah"},
+		{"https://hdl.handle.net/XXXXX/test/bl ah", "https=u003A/hdl.handle.net/XXXXX/test/bl=u0020ah"},
+		{"abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij abcdefghijabcdefghij", "fallback/b/8/b8acda4abac53237afa03d6bbb078e1bf46b40438bb256df79b8d9ff0e57b32a688156ad21755363ea19953c160c4dd6d4db175b71e9aa87d68937181a9f69d/9"},
+	})
+}
